399c7a7ead17c1fd7b29ba5c9daaf0f74f193c6c
kishanraaj77 <9276622+kishanraaj77@users.noreply.github.com>
2021-03-24 10:29:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,39 @@
-from flask import Flask,request,Response
+rom flask import Flask,request,Response
 from botbuilder.core import BotFrameworkAdapter,BotFrameworkAdapterSettings,TurnContext,ConversationState,MemoryStorage
- 
+from botbuilder.schema import Activity
 import asyncio
-from pic_button import SampleAnimationCard
-
-from aiohttp import web
-from aiohttp.web import Request, Response, json_response
-
-from botbuilder.core.integration import aiohttp_error_middleware
-from botbuilder.schema import Activity, ActivityTypes
+from hero_card import SampleAnimationCard
 
 
 app = Flask(__name__)
 loop = asyncio.get_event_loop()
 
-botsettings = BotFrameworkAdapterSettings("053b477c-6f0b-4629-a12d-6bef846605ac","2da80739-ab3a-4fb1-8fa1-c4df64b087f2")
+botsettings = BotFrameworkAdapterSettings("","")
 botadapter = BotFrameworkAdapter(botsettings)
 
 CONMEMORY = ConversationState(MemoryStorage())
 botdialog = SampleAnimationCard()
 
- 
 
-    
-
-# Listen for incoming requests on /api/messages
-async def messages(req: Request) -> Response:
-    # Main bot message handler.
-    if "application/json" in req.headers["Content-Type"]:
-        body = await req.json()
+@app.route("/api/messages",methods=["POST"])
+def messages():
+    if "application/json" in request.headers["content-type"]:
+        body = request.json
     else:
-        return Response(status=415)
+        return Response(status = 415)
 
     activity = Activity().deserialize(body)
-    auth_header = req.headers["Authorization"] if "Authorization" in req.headers else ""
 
-    try:
-        response = await botadapter.process_activity(activity, auth_header, botdialog.on_turn)
-        if response:
-            return json_response(data=response.body, status=response.status)
-        return Response(status=201)
-    except Exception as exception:
-        raise exception
+    auth_header = (request.headers["Authorization"] if "Authorization" in request.headers else "")
+
+    async def call_fun(turncontext):
+        await botdialog.on_turn(turncontext)
+
+    task = loop.create_task(
+        botadapter.process_activity(activity,auth_header,call_fun)
+        )
+    loop.run_until_complete(task)
 
 
-APP = web.Application(middlewares=[aiohttp_error_middleware])
-APP.router.add_post("/api/messages", messages)
-
-if __name__ == "__main__":
-    try:
-        web.run_app(APP, host="localhost", port=CONFIG.PORT)
-    except Exception as error:
-        raise error
+if __name__ == '__main__':
+    app.run('localhost',3978)
